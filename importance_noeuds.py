23bf0d83e2a8189144768565a9578de0ebc46dc3
AMEDOMEY Roméo David <davidromeoamedomey@yahoo.com>
2023-01-08 10:48:03 +0100

    evolution des noeuds importants dans les k-coeur

--- importance_noeuds.py
+++ importance_noeuds.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import random
 
 import networkx as nx
 from matplotlib import pyplot as plt
@@ -19,28 +18,28 @@ print(g)
 node_importance = get_node_importance(g)
 
 ################################
-# # Dans le coeur ou la périphérie
-# in_core = [0, 0]
-# in_periph = [0, 0]
-#
-# # noyau principal
-# principal_core_nodes = nx.k_core(g).nodes
-# print("nbr de noeuds importants", len(node_importance))
-# print("max score ", max(node_importance.values()))
-# for node, importance in node_importance.items():
-#     if importance >= max(node_importance.values()) * 0.5:
-#         if node in principal_core_nodes:
-#             in_core[0] += 1
-#         else:
-#             in_periph[0] += 1
-#     else:
-#         if node in principal_core_nodes:
-#             in_core[1] += 1
-#         else:
-#             in_periph[1] += 1
-#
-# print("score >=1/2 max(score): in_core = ", in_core[0], " in_periph = ", in_periph[0])
-# print("score <1/2 max(score): in_core = ", in_core[1], " in_periph = ", in_periph[1])
+# Dans le coeur ou la périphérie
+in_core = [0, 0]
+in_periph = [0, 0]
+
+# noyau principal
+principal_core_nodes = nx.k_core(g).nodes
+print("nbr de noeuds importants", len(node_importance))
+print("max score ", max(node_importance.values()))
+for node, importance in node_importance.items():
+    if importance >= max(node_importance.values()) * 0.5:
+        if node in principal_core_nodes:
+            in_core[0] += 1
+        else:
+            in_periph[0] += 1
+    else:
+        if node in principal_core_nodes:
+            in_core[1] += 1
+        else:
+            in_periph[1] += 1
+
+print("score >=1/2 max(score): in_core = ", in_core[0], " in_periph = ", in_periph[0])
+print("score <1/2 max(score): in_core = ", in_core[1], " in_periph = ", in_periph[1])
 
 ##################################
 # Le k-coeur spécifique d'appartenance
